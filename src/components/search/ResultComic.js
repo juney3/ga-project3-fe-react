0bac0384fcff40330aaf7e0375d9c09e2e92c2bb
juney3 <juney37@gmail.com>
2018-07-10 16:36:24 -0700

    sort of finalized result styles

--- src/components/search/ResultComic.js
+++ src/components/search/ResultComic.js
@@ -1,61 +1,32 @@
 import React, { Component } from 'react';
-import { Button, Card, Form, Header, Image, Table } from 'semantic-ui-react';
+import { Button, Card, Form, Grid, Header, Image, List } from 'semantic-ui-react';
 
 export default class ResultComic extends Component {
   render() {
     let comicUrl=this.props.result.urls[0].url
-    let date = this.props.result.dates[0].date
+    let date = this.props.result.dates[0].date.substring(0,9)
     let imgUrl=`${this.props.result.thumbnail.path}/portrait_xlarge.${this.props.result.thumbnail.extension}`
     return(
       <Card>
         <Card.Content>
-          <Image floated='right' size='small' src={imgUrl} />
+          <Card.Description>
           <a href={comicUrl} target='new'>
-            <Card.Header as='h4'>
+            <Card.Header as='h2'>
               {this.props.result.title}
             </Card.Header>
           </a>
+          </Card.Description>
           <Card.Description>
-            <Table basic='very' celled collapsing>
-              <Table.Body>
-                <Table.Row>
-                  <Table.Cell>
-                    <Header as='h5'>
-                      <Header.Content>
-                        Issue #
-                      </Header.Content>
-                    </Header>
-                  </Table.Cell>
-                  <Table.Cell>
-                    {this.props.result.issueNumber}
-                  </Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell>
-                    <Header as='h5'>
-                      <Header.Content>
-                        Sale date
-                      </Header.Content>
-                    </Header>
-                  </Table.Cell>
-                  <Table.Cell>
-                    {date}
-                  </Table.Cell>
-                </Table.Row>
-                <Table.Row>
-                  <Table.Cell>
-                    <Header as='h5'>
-                      <Header.Content>
-                        Description
-                      </Header.Content>
-                    </Header>
-                  </Table.Cell>
-                  <Table.Cell>
-                    {this.props.result.description}
-                  </Table.Cell>
-                </Table.Row>
-              </Table.Body>
-            </Table>
+            <Image floated='right' size='small' src={imgUrl} />
+            <Card.Header as='h3'>
+              Issue #: {this.props.result.issueNumber}
+            </Card.Header>
+            <Card.Header as='h3'>
+              Sale date: {date}
+            </Card.Header>
+          </Card.Description>
+          <Card.Description>
+            {this.props.result.description}
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -16,51 +16,60 @@ export default class Search extends Component {
 
   render() {
     return(
-      <div className="searchForm">
+      <div>
+        <div className="searchForm">
+          <Grid centered columns={2}>
+            <Grid.Column>
+              <Form>
+                <Header as='h2'>
+                  Search for comics by character
+                </Header>
+                <Form.Field>
+                  <label>Character name</label>
+                  <Input
+                    name="characterName"
+                    onChange={this.props.handleInput}
+                    placeholder="Enter a character name"/>
+                </Form.Field>
+                <Header as='h5'>Enter a date range (optional)</Header>
+                <Form.Group widths='equal'>
+                  <Form.Field>
+                    <label>Start year</label>
+                    <Input
+                      name='startYear'
+                      onChange={this.props.handleInput}
+                      placeholder="Enter a starting year"/>
+                  </Form.Field>
+                  <Form.Field>
+                    <label>End year</label>
+                    <Input
+                      name='endYear'
+                      onChange={this.props.handleInput}
+                      placeholder="Enter an ending year"/>
+                  </Form.Field>
+                </Form.Group>
+                <Button
+                  type='submit'
+                  onClick={this.props.handleSearch}>
+                    Search
+                </Button>
+              </Form>
+            </Grid.Column>
+          </Grid>
+
+          <Results
+            handleInput={this.props.handleInput}
+            selectedList={this.props.selectedList}
+            searchResults={this.props.searchResults}
+          />
+        </div>
         <Grid centered columns={2}>
-          <Grid.Column>
-            <Form>
-              <Header as='h2'>
-                Search for comics by character
-              </Header>
-              <Form.Field>
-                <label>Character name</label>
-                <Input
-                  name="characterName"
-                  onChange={this.props.handleInput}
-                  placeholder="Enter a character name"/>
-              </Form.Field>
-              <Header as='h5'>Enter a date range (optional)</Header>
-              <Form.Group widths='equal'>
-                <Form.Field>
-                  <label>Start year</label>
-                  <Input
-                    name='startYear'
-                    onChange={this.props.handleInput}
-                    placeholder="Enter a starting year"/>
-                </Form.Field>
-                <Form.Field>
-                  <label>End year</label>
-                  <Input
-                    name='endYear'
-                    onChange={this.props.handleInput}
-                    placeholder="Enter an ending year"/>
-                </Form.Field>
-              </Form.Group>
-              <Button
-                type='submit'
-                onClick={this.props.handleSearch}>
-                  Search
-              </Button>
-            </Form>
+          <Grid.Column textAlign="center">
+            <footer >
+              <a href="http://marvel.com" target="new">Data provided by Marvel. © 2018 MARVEL</a>
+            </footer >
           </Grid.Column>
         </Grid>
-
-        <Results
-          handleInput={this.props.handleInput}
-          selectedList={this.props.selectedList}
-          searchResults={this.props.searchResults}
-        />
       </div>
     )
   }
